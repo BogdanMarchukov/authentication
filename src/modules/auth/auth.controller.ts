4c902de6eec3354713ffb2d3d93a0d9f93a5292a
bogdan.marchukov <bogdan.marchukov@wilix.ru>
2023-04-16 22:48:43 +0300

    login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { UserDto } from '../user/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  login(@Body(new ValidationPipe()) userDto: UserDto) {
+    return this.authService.signIn(userDto.login, userDto.password);
+  }
+}
